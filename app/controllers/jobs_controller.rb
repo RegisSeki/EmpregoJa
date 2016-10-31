181f5b72582385235c42eee57ad9cd658ac543ce
Regis_Daniel <regisseki@gmail.com>
2016-10-31 21:44:37 -0200

    user create job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,20 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    job = Job.create(jobs_params)
+    redirect_to job_url(job)
+  end
+
+  private
+
+  def jobs_params
+    params.require(:job)
+          .permit(:title, :location, :category, :company, :description)
+  end
 end
