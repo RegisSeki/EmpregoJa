a8f826079bb405364543f433cacc59878590ca8b
Regis_Daniel <regisseki@gmail.com>
2016-11-09 20:14:53 -0200

    Usuario altera categoria

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,15 +1,35 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :edit, :update]
+
   def new
     @company = Company.new
   end
 
   def create
-    @company = Company.create(params.require(:company)
-                                    .permit(:name, :location, :mail, :phone))
+    @company = Company.create(category_params)
     redirect_to company_url(@company)
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @company.update(category_params)
+
+    redirect_to @company
+  end
+
+  private
+
+  def set_company
     @company = Company.find(params[:id])
+  end
+
+  def category_params
+    params.require(:company)
+          .permit(:name, :location, :mail, :phone)
   end
 end
---
+++ spec/features/user_edit_company_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User edit company' do
+  scenario 'successfully'  do
+    company = Company.create(name: 'Campus Code',
+                             location: 'São Paulo',
+                             mail: 'contato@campus.com.br',
+                             phone: '2369-3476')
+
+
+    visit edit_company_path(company)
+
+    fill_in 'Nome', with: 'eduK'
+    fill_in 'Local', with: 'Belo Horizonte'
+    fill_in 'Email', with: 'banana@banana.com.br'
+    fill_in 'Telefone', with: '5555-5555'
+
+    click_on 'Atualizar'
+
+    expect(page).to have_css('h1', text: 'eduK')
+    expect(page).to have_content 'Belo Horizonte'
+    expect(page).to have_content 'banana@banana.com.br'
+    expect(page).to have_content '5555-5555'
+  end
+
+
+end
