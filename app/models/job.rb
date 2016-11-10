520c4fc367cc759ab64c5ba259c207df9c007346
Regis_Daniel <regisseki@gmail.com>
2016-11-09 22:04:59 -0200

    Foi aplicada a regra de expiração do model Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,9 @@
 class Job < ApplicationRecord
   belongs_to :company
   belongs_to :category
-  validates :title, presence: true
+  validates :title, :location, :description, :category, presence: true
+
+  def expired?
+    90.days.ago > created_at
+  end
 end
